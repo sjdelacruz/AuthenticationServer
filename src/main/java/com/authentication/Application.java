0d3b3f1ccb0079dbfa313e4dbde6cdbea8bff15c
sdelacruz <sdelacruz.mca@gmail.com>
2024-07-17 00:40:33 -0600

    Avances en pruebas de oauth token.

--- src/main/java/com/authentication/Application.java
+++ src/main/java/com/authentication/Application.java
@@ -1,4 +1,4 @@
-package com.authentication.com.authentication;
+package com.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
